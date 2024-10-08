c7d0597065e80b3b90b3ab4e0901603e856866ca
Bhairavi Sawantdesai <51810658+BhairaviVSD@users.noreply.github.com>
2024-10-08 02:10:23 -0400

    Time: 779 ms (26.60%) | Memory: 27.3 MB (98.30%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,8 @@ class Solution:
         right = 1
         maxProfit = 0
         while right < len(prices) :
-            print(left, right, prices[right], prices[left])
             if (prices[left] < prices[right]):
                 maxProfit = max(maxProfit, prices[right]-prices[left])
-                print(prices[right], prices[left])
             else:
                 left = right
             right = right + 1
